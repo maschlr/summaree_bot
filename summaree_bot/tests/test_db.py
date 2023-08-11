53041f4ee58c2d3633c4d39b824a9e105080b804
Marten (msc) <msc@odoo.com>
2023-08-11 09:14:24 +0200

    add simple test with data from traceback, script to delete all users and chats

--- summaree_bot/tests/test_db.py
+++ summaree_bot/tests/test_db.py
@@ -1,3 +1,5 @@
+import datetime
+
 from summaree_bot.models import TelegramUser
 
 from .common import Common
@@ -18,3 +20,22 @@ class TestDb(Common):
 
         tg_user = session.get(TelegramUser, user_id)
         self.assertEqual(tg_user.first_name, "user_new")
+
+    def test_01_big_int(self):
+        vals = {
+            "id": 6633528690,
+            "is_bot": False,
+            "first_name": "Shiva",
+            "last_name": "Schiff",
+            "username": None,
+            "language_code": "en",
+            "is_premium": None,
+            "created_at": datetime.datetime(2023, 8, 10, 20, 6, 0, 314647),
+            "updated_at": datetime.datetime(2023, 8, 10, 20, 6, 0, 314706),
+        }
+        with self.Session.begin() as session:
+            tg_user = TelegramUser(**vals)
+            session.add(tg_user)
+
+        tg_user = session.get(TelegramUser, vals["id"])
+        self.assertEqual(tg_user.id, vals["id"])
